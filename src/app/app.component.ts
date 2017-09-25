1ea7a9f8c09d6b2a050d8b03ed2a6e20d3c86182
MyGps5-PC\MyGps5 <sturua1010@gmail.com>
2017-09-25 12:02:24 +0400

    add preloader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  public isLoading: boolean = true;
+  ngOnInit(){
+    console.log(this.isLoading);
+    setTimeout(() => { 
+      this.isLoading = false; 
+      console.log(this.isLoading); 
+    }, 3000);
+  }
 }
+
